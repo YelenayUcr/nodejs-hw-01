d4035fbdc638a83853a686e213ea55987cbf7acf
Yelenay Uçar <yelenayucr@gmail.com>
2025-05-20 21:56:38 +0300

    UPDATE

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,12 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try {
+        const data = await fs.readFile('src/db/db.json', 'utf-8');
+        console.log('Data read from file:', data);
+    }
+    catch (error) {
+        console.error('Error reading contacts:', error);
+    }
+};
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,9 @@
 import { PATH_DB } from '../constants/contacts.js';
-
-export const writeContacts = async (updatedContacts) => {};
+import fs from 'fs/promises';
+export const writeContacts = async (updatedContacts) => {
+    try {
+        await fs.writeFile('src/db/db.json', 'utf-8'); 
+    }catch (error) {
+        console.error('Error writing contacts:', error);
+    }
+};
