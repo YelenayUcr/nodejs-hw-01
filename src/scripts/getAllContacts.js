e347d965453bbf701f7b53f51078f20651de6708
Yelenay Uçar <yelenayucr@gmail.com>
2025-05-24 19:43:30 +0300

    Update getAllContacts.js

--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -2,18 +2,11 @@ import { readContacts } from "../utils/readContacts.js";
 
 
 export const getAllContacts = async () => {
-    try {
-        const contacts = await readContacts();
-        return contacts;
-    } catch (error) {
-        console.error('Error getting all contacts:', error);
-        throw error;
-    }   
-};
-
-const main = async () => {
-    const allContacts = await getAllContacts();
-    console.log(allContacts);
-};
-
-main();
+    const contacts = await readContacts(); 
+ 
+  
+    console.log(contacts); 
+ 
+  };
+  
+  getAllContacts(); 
